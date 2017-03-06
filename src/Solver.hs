eb3b5c9ca2290229d0522aed21f32652dff3fa73
Federico Rampazzo <frampone@gmail.com>
2017-03-06 05:18:01 +0000

    Improved solver, follow requests score order

--- src/Solver.hs
+++ src/Solver.hs
@@ -6,8 +6,8 @@ import Debug.Trace
 import qualified Data.IntMap as IntMap
 import qualified Type as T
 
-requestProblems :: T.State -> (Int, T.Request) -> [T.Problem]
-requestProblems state (requestIndex, request) = 
+makeProblems :: T.State -> (Int, T.Request) -> [T.Problem]
+makeProblems state (requestIndex, request) = 
   let
     requestSize = T.size request
     videoIndex = T.videoIndex (request :: T.Request)
@@ -19,24 +19,15 @@ requestProblems state (requestIndex, request) =
   in
     map makeProblem (T.cachesLatencies endpoint)
 
-insertRequests :: [T.Problem] -> (T.CacheIndex, T.Cache) -> IntMap.IntMap [T.VideoIndex]
-insertRequests problems (cacheIndex, cacheSize) =
-  let
-    filterByCache = filter $ (== cacheIndex) . T.cacheIndex
-    sortByScore = sortOn $ (* (-1)) . T.score
-    -- Add check if req is already satisfied, bigger score wins
-    -- Check size
-    insertRequest problem = IntMap.insertWith (++) (T.cacheIndex problem) [T.videoIndex (problem :: T.Problem)] 
-  in
-    foldl (flip insertRequest) IntMap.empty ((sortByScore . filterByCache) problems)
-
 makeAllocations :: T.State -> IntMap.IntMap [T.VideoIndex]
 makeAllocations state = 
   let 
-    problems = foldl (++) [] $ map (requestProblems state) $ zip [0..] (T.requests state)
-    cacheAllocationMaps = map (insertRequests problems) $ T.caches state
+    problems = foldl (++) [] $ map (makeProblems state) $ zip [0..] (T.requests state)
+    sortByScore = sortOn $ (* (-1)) . T.score
+    --TODO: Check videoSize against cacheSize
+    solveProblem problem = IntMap.insertWith (++) (T.cacheIndex problem) [T.videoIndex (problem :: T.Problem)] 
   in 
-    foldl (IntMap.unionWith (++)) IntMap.empty cacheAllocationMaps
+    foldl (flip solveProblem) IntMap.empty (sortByScore problems)
 
 solve :: T.State -> T.State
 solve state = state { T.allocations = makeAllocations state }
