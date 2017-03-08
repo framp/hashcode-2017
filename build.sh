908e81c432ba0bddfc76ce2e743de862b951b94f
Federico Rampazzo <frampone@gmail.com>
2017-03-08 00:10:42 +0000

    Improved build script

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+echo 'ghc -O2 -isrc --make -threaded -rtsopts -o hashcode src/Main.hs'
 ghc -O2 -isrc --make -threaded -rtsopts -o hashcode src/Main.hs
+echo 'rm src/*.o src/*.hi'
+rm src/*.o src/*.hi
